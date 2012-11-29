9d3ea4f96356bc5cbe85e92f0f33901f9e067637
Shankar Cabus <shankarcabus@gmail.com>
2012-11-28 22:19:21 -0200

    Added LaTeX files

---
+++ monografia/clean.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+erase *.aux
+erase *.lof
+erase *.lot
+erase *.toc
+erase *.dvi
+erase *.log
+erase *.pdf
+erase *.blg
+erase *.bbl
+erase *.synctex
+erase *.ps
+erase *.ist
+erase *.glo
+
+@ECHO Clean successfull
